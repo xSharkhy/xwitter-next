bec0d5485240d4da9773f16fc06c1bafc2b8422b
xSharkhy <sharkhyacc@gmail.com>
2023-09-26 14:30:16 +0200

    chore(auth): add OAuth with Github
    - created button components
    - handled conditional rendering on components
    - added API for OAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home () {
+import { AuthButtonServer } from '@/components/AuthButton-Server'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export default async function Home () {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: posts } = await supabase.from('posts').select('*')
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hola Xwitter 👋
+      <AuthButtonServer />
+      <pre>
+        {
+          JSON.stringify(posts, null, 2)
+        }
+      </pre>
     </main>
   )
 }
